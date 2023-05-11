f3b8f0f22a71de87c5362deb5a9dba117e3e4ec4
MariaBetancur <mabo6241@gmail.com>
2023-05-10 22:15:00 -0500

    v1.2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,12 @@
+// Change code below this line
 const gallery = document.querySelector('.gallery');
-
 gallery.addEventListener('click', (event) => {
   event.preventDefault();
   const imageSrc = event.target.dataset.source;
-  if (!imageSrc) return;
+  target.nodeName !== "IMG";
   const instance = basicLightbox.create(`
     <img src="${imageSrc}" width="800" height="600">
   `);
   instance.show(); // Mostramos la ventana modal
 });
+console.log(galleryItems);

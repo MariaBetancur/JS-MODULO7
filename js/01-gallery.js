c7b64003ba36b035afb5601717105c73e8d928c1
MariaBetancur <mabo6241@gmail.com>
2023-05-11 22:15:10 -0500

    v1.2.2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,31 @@
-// Change code below this line
 import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML(
+  'beforeend',
+  galleryItems
+    .map(({ preview, original, description }) => `
+      <div class="gallery_items">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>
+    `)
+    .join('')
+);
 gallery.addEventListener('click', (event) => {
   event.preventDefault();
-  const imageSrc = event.target.dataset.source;
-  target.nodeName !== "IMG";
+  const target = event.target;
+  if (target.nodeName !== 'IMG') return;
+  const imageSrc = target.dataset.source;
   const instance = basicLightbox.create(`
     <img src="${imageSrc}" width="800" height="600">
   `);
-  instance.show(); // Mostramos la ventana modal
+  instance.show();
 });
+
 console.log(galleryItems);
